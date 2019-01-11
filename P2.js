7b23dbc34ac0165b43c9837df4aec9742cdffd61
PorcheWooten <porchewooten@yahoo.com>
2019-01-11 12:45:15 -0600

    classworkUpdate

---
+++ P2.js
@@ -0,0 +1,6 @@
+// Problem 2 Create a variable called nameVariable. Print "Hello " + nameVariable
+
+var nameVar = "Porche";
+{
+    console.log("Hello" + " " + nameVar)
+}
---
+++ P4.js
@@ -0,0 +1,3 @@
+// Problem 4 Print "1 + 1 = " and the answer.
+
+console.log('1+1=' + (1+1))
